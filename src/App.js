50167eb65c347097651f16ffe32bbf9ca19a4ff7
Calvin Shio <shiocorp49@gmail.com>
2024-03-28 18:31:49 +0300

    feat-> cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Analytics from "./Components/Analytics";
 import NewsLetter from "./Components/NewsLetter";
+import Cards from "./Components/Cards";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Hero/>
             <Analytics />
             <NewsLetter />
+            <Cards />
         </div>
     );
 }
---
+++ src/Components/Cards.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Single from '../assets/single.png'
+import Double from '../assets/double.png'
+import Triple from '../assets/triple.png'
+function Cards(props) {
+    return (
+        <div className={"w-full py-[10rem] px-4 bg-white"}>
+            <div className={"max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8"}>
+                <div className={"w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300"}>
+                    <img src={Single} className={"w-20 mx-auto mt-[-3rem]"} alt="/"/>
+                    <h2 className={"text-2xl font-bold text-center py-8"}>Single User</h2>
+                    <p className={"text-center text-4xl font-bold"}>$149</p>
+                    <div className={"text-center font-medium"}>
+                        <p className={"border-b mx-8 mt-8 py-2"}>500 GB Storage</p>
+                        <p className={"border-b mx-8 mt-8 py-2"}>1 Granted User</p>
+                        <p className={"border-b mx-8 mt-8 py-2"}>Send Upto 2 GB</p>
+                        <p className={"border-b mx-8 mt-8 py-2"}>500GB Storage</p>
+                    </div>
+                    <button className={"bg-[#00fd9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3"}>Start Trial</button>
+                </div>
+                <div className={"w-full shadow-xl bg-gray-100 flex flex-col p-4 my-8 md:m-0 rounded-lg hover:scale-105 duration-300"}>
+                    <img src={Double} className={"w-20 mx-auto mt-[-3rem] bg-transparent"} alt="/"/>
+                    <h2 className={"text-2xl font-bold text-center py-8"}>Two Users</h2>
+                    <p className={"text-center text-4xl font-bold"}>$149</p>
+                    <div className={"text-center font-medium"}>
+                        <p className={"border-b mx-8 mt-8 py-2"}>500 GB Storage</p>
+                        <p className={"border-b mx-8 mt-8 py-2"}>1 Granted User</p>
+                        <p className={"border-b mx-8 mt-8 py-2"}>Send Upto 2 GB</p>
+                        <p className={"border-b mx-8 mt-8 py-2"}>500GB Storage</p>
+                    </div>
+                    <button className={"text-[#00fd9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 bg-black"}>Start Trial</button>
+                </div>
+                <div className={"w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300"}>
+                    <img src={Triple} className={"w-20 mx-auto mt-[-3rem]"} alt="/"/>
+                    <h2 className={"text-2xl font-bold text-center py-8"}>Three User</h2>
+                    <p className={"text-center text-4xl font-bold"}>$149</p>
+                    <div className={"text-center font-medium"}>
+                        <p className={"border-b mx-8 mt-8 py-2"}>500 GB Storage</p>
+                        <p className={"border-b mx-8 mt-8 py-2"}>1 Granted User</p>
+                        <p className={"border-b mx-8 mt-8 py-2"}>Send Upto 2 GB</p>
+                        <p className={"border-b mx-8 mt-8 py-2"}>500GB Storage</p>
+                    </div>
+                    <button className={"bg-[#00fd9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3"}>Start Trial</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Cards;
