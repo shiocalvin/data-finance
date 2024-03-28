32591369b4cbc79af7fbb404b03a0ca0bc8fb401
Calvin Shio <shiocorp49@gmail.com>
2024-03-28 16:51:03 +0300

    feat-> analytics page completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Analytics from "./Components/Analytics";
 
 function App() {
     return (
         <div>
             <Navbar/>
             <Hero/>
+            <Analytics />
         </div>
     );
 }
