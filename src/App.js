5dce8dc915269186dd60638dabc2b32b1b248695
Calvin Shio <shiocorp49@gmail.com>
2024-03-28 17:34:06 +0300

    feat-> newsletter section complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Analytics from "./Components/Analytics";
+import NewsLetter from "./Components/NewsLetter";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Navbar/>
             <Hero/>
             <Analytics />
+            <NewsLetter />
         </div>
     );
 }
